dd1e9dff99db427cd43907fbfb925a8b59d5cd05
Garrett James <garrettjames@Garretts-MBP.home>
2021-09-03 10:31:56 -0400

    fixed a corrupt repo

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Directory from '../../components/Directory/Directory';
+
+import './HomePage.scss';
+
+const Hompage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default Hompage;
